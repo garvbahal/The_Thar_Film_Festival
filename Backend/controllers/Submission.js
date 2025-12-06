64d43fd13d4dca8f13af9b9d1d28989da43d232a
garvbahal <garvbahal75@gmail.com>
2025-12-07 01:22:05 +0530

    submitLink

--- Backend/controllers/Submission.js
+++ Backend/controllers/Submission.js
@@ -33,3 +33,49 @@ exports.getTeamDetails = async (req, res) => {
         });
     }
 };
+
+exports.submitLink = async (req, res) => {
+    try {
+        const { driveLink, youtubeLink } = req.body;
+        if (!driveLink && !youtubeLink) {
+            return res.status(400).json({
+                success: false,
+                message:
+                    "Please provide at least a Google Drive link or a YouTube link.",
+            });
+        }
+
+        const userId = req.user.id;
+        const user = await User.findById(userId);
+        const teamId = user.team;
+
+        const team = await Team.findById(teamId);
+
+        if (!team) {
+            return res.status(404).json({
+                success: false,
+                message: "Team not found",
+            });
+        }
+
+        team.submission = {
+            driveLink: driveLink,
+            youtubeLink: youtubeLink,
+            submittedAt: new Date(),
+        };
+
+        await team.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Submission uploaded successfully",
+            submission: team.submission,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong while uploading submit link",
+            error: error.message,
+        });
+    }
+};
